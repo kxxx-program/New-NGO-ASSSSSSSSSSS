ef85d998298d942c0b107bb67f07af0927bd8c04
Danieal3223 <danieallianeujiun@gmail.com>
2025-09-20 04:16:29 +0800

    Events DB Table Fix

---
+++ Migrations/20250919200712_AddEventTimeColumns.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NGO_Web_Demo.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEventTimeColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "EventStartTime",
+                table: "Events",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "EventEndTime",
+                table: "Events",
+                type: "time",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EventStartTime",
+                table: "Events");
+
+            migrationBuilder.DropColumn(
+                name: "EventEndTime",
+                table: "Events");
+        }
+    }
+}
